15a4a41c18dbc9f9dd8db239f5529ef8ed8ab410
xinyun <3161512889@qq.com>
2023-07-21 21:57:34 +0800

    静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,6 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-    <div>
-        我是首页
-        <router-view></router-view>
-    </div>
+    <LayoutNav />
+    <LayoutHeader />
+    <RouterView />
+    <LayoutFooter />
 </template>
