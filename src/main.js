a0d685325b9796537aeef156346a7f5139ad32a8
xinyun <3161512889@qq.com>
2023-07-22 11:52:58 +0800

    layout-吸顶导航交互实现

--- src/main.js
+++ src/main.js
@@ -6,10 +6,6 @@ import router from './router'
 
 import '@/styles/common.scss'
 
-import { getCategory } from '@/apis/testAPI'
-getCategory().then(res => {
-    console.log(res)
-})
 const app = createApp(App)
 
 app.use(createPinia())
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,4 @@
 <template>
     我是home页
+    <div style="height: 1200px;"></div>
 </template>
