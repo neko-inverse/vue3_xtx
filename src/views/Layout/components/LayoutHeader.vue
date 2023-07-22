df182997b6b76d5d4cb889ff6f88143da4eecc64
xinyun <3161512889@qq.com>
2023-07-22 14:28:46 +0800

    home-整体结构拆分以及分类的数据渲染

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -11,6 +11,9 @@ const categoryStore = useCategoryStore()
                 <RouterLink to="/">小兔鲜</RouterLink>
             </h1>
             <ul class="app-header-nav">
+                <li class="home">
+                        <RouterLink to="/">首页</RouterLink>
+                </li>
                 <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
                     <RouterLink to="/">{{ item.name }}</RouterLink>
                 </li>
