7288b1d1cae8d089be03c4a3bdba281e666fab23
xinyun <3161512889@qq.com>
2023-07-27 11:36:08 +0800

    登录-表单校验-统一校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -17,7 +17,6 @@ const rules = {
     agree: [
         {
             validator: (rule,value,callback) => {
-                console.log(value)
                 // 自定义校验逻辑
                 if (value) {
                     callback()
@@ -27,6 +26,16 @@ const rules = {
             }
         }
     ]
+}
+
+// 获取form实例统一校验
+const formRef = ref(null)
+const doLogin = () => {
+    formRef.value.validate((valid) => {
+        if (valid) {
+            
+        }
+    })
 }
 </script>
 
@@ -52,7 +61,7 @@ const rules = {
                 </nav>
                 <div class="account-box">
                     <div class="form">
-                        <el-form :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
+                        <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
                             <el-form-item prop="account" label="账户">
                                 <el-input v-model="form.account" />
                             </el-form-item>
@@ -64,7 +73,7 @@ const rules = {
                                     我已同意隐私条款和服务条款
                                 </el-checkbox>
                             </el-form-item>
-                            <el-button size="large" class="subBtn">点击登录</el-button>
+                            <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
                         </el-form>
                     </div>
                 </div>
