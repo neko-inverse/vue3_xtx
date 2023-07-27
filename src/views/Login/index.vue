e8cc6d615d4d14431cdeeda309cb2cdba75f806f
xinyun <3161512889@qq.com>
2023-07-27 11:01:17 +0800

    登录-表单校验-自定义校验规则

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -13,6 +13,19 @@ const rules = {
     password: [
         { required: true, message: '密码不能为空', trigger: 'blur' },
         { min: 6, max: 14, message: '密码长度为6-14个字符', trigger: 'blur' }
+    ],
+    agree: [
+        {
+            validator: (rule,value,callback) => {
+                console.log(value)
+                // 自定义校验逻辑
+                if (value) {
+                    callback()
+                } else {
+                    callback(new Error('请勾选协议'))
+                }
+            }
+        }
     ]
 }
 </script>
@@ -46,8 +59,8 @@ const rules = {
                             <el-form-item prop="password" label="密码">
                                 <el-input v-model="form.password" />
                             </el-form-item>
-                            <el-form-item label-width="22px">
-                                <el-checkbox size="large">
+                            <el-form-item prop="agree" label-width="22px">
+                                <el-checkbox size="large" v-model="form.agree">
                                     我已同意隐私条款和服务条款
                                 </el-checkbox>
                             </el-form-item>
