502e19e2a9b21ec84e973b5d12cf0f4ee7e210f2
xinyun <3161512889@qq.com>
2023-07-28 13:00:04 +0800

    401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
-import 'element-plus/theme-chalk/el-message.css'
 import { ElMessage } from 'element-plus'
 import { useUserStore } from '@/stores/user'
+import router from '@/router'
 
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -20,11 +20,17 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+  const userStore = useUserStore()
   // 统一错误提示
   ElMessage({
     type: 'warning',
     message: e.response.data.msg
   })
+  // 401 token失效处理
+  if (e.response.status === 401) {
+    userStore.clearUserInfo()
+    router.push(('/login'))
+  }
   return Promise.reject(e)
 })
 
