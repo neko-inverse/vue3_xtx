abe92f82f4a5e456d5f90c8a12c4661404d02025
xinyun <3161512889@qq.com>
2023-07-29 14:44:42 +0800

    接口购物车-删除功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -2,18 +2,22 @@
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
 import { useUserStore } from './user'
-import { insertCartAPI, findNewCartListAPI } from '@/apis/cart'
+import { insertCartAPI, findNewCartListAPI, delCartAPI } from '@/apis/cart'
 
 export const useCartStore = defineStore('cart', () => {
   const userStore = useUserStore()
   const isLogin = computed(() => userStore.userInfo.token)
   const cartList = ref([])
+  // 获取最新的购物车列表action
+  const updateNewList = async () => {
+    const res = await findNewCartListAPI()
+    cartList.value = res.result
+  }
   const addCart = async (goods) => {
     const { skuId, count } = goods
     if (isLogin.value) {
       await insertCartAPI({ skuId, count })
-      const res = await findNewCartListAPI()
-      cartList.value = res.result
+      updateNewList()
     } else {
       const item = cartList.value.find((item) => goods.skuId === item.skuId)
       if (item) {
@@ -26,9 +30,15 @@ export const useCartStore = defineStore('cart', () => {
   }
 
   // 删除购物车
-  const delCart = (skuId) => {
-    const idx = cartList.value.findIndex((item) => skuId === item.skuId)
-    cartList.value.splice(idx,1)
+  const delCart = async (skuId) => {
+    if (isLogin.value) {
+      await delCartAPI([skuId])
+      updateNewList()
+    } else {
+      const idx = cartList.value.findIndex((item) => skuId === item.skuId)
+      cartList.value.splice(idx,1)
+    }
+    
   }
 
   // 单选功能
