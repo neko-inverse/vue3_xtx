55d19d57df21fdff446e3e6029de653aa5a26d12
xinyun <3161512889@qq.com>
2023-07-30 11:51:43 +0800

    结算页-提交订单业务实现

--- src/apis/checkout.js
+++ src/apis/checkout.js
@@ -5,3 +5,12 @@ export const getCheckInfoAPI = () => {
     url: '/member/order/pre'
   })
 }
+
+// 创建订单
+export const createOrderAPI = (data) => {
+  return request({
+    url: '/member/order',
+    method: 'POST',
+    data
+  })
+}
