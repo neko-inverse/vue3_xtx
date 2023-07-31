2a4558b7e316efcaa6570f12e542001530cca2ac
xinyun <3161512889@qq.com>
2023-07-31 11:23:43 +0800

    会员中心-订单-使用基础参数获取基础列表

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { getUserOrder } from '@/apis/order'
+import { ref, onMounted } from 'vue'
 // tab列表
 const tabTypes = [
   { name: "all", label: "全部订单" },
@@ -9,9 +11,18 @@ const tabTypes = [
   { name: "complete", label: "已完成" },
   { name: "cancel", label: "已取消" }
 ]
-// 订单列表
-const orderList = []
-
+// 获取订单列表
+const orderList = ref([])
+const params = ref({
+  orderState: 0,
+  page: 1,
+  pageSize: 2
+})
+const getOrderList = async () => {
+  const res = await getUserOrder(params.value)
+  orderList.value = res.result.items
+}
+onMounted(() => getOrderList())
 </script>
 
 <template>
