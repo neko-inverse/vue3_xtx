f3f2f14c1f6c6c43287380ec018a947d9b1a00ce
xinyun <3161512889@qq.com>
2023-07-31 15:05:44 +0800

    会员中心-订单-使用tab切换获取状态数据

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -23,11 +23,16 @@ const getOrderList = async () => {
   orderList.value = res.result.items
 }
 onMounted(() => getOrderList())
+
+const tabChange = (type) => {
+  params.value.orderState = type
+  getOrderList()
+}
 </script>
 
 <template>
   <div class="order-container">
-    <el-tabs>
+    <el-tabs @tab-change="tabChange">
       <!-- tab切换 -->
       <el-tab-pane v-for="item in tabTypes" :key="item.name" :label="item.label" />
 
